83c574b86ffde979e4541c710e93a5ee578e0f1b
shanekenyon87 <shane.kenyon@redargyle.com>
2018-10-04 13:42:12 -0400

    Added uploadFile function

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const functions = require('firebase-functions');
 admin.initializeApp(functions.config().firebase);
 
 var db = admin.firestore();
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.uploadFile = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
